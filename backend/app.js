aff3043d8a30173582bea2e3c6b7962a3dc10365
Coen Steuns <c.steuns@gmail.com>
2020-08-24 16:26:08 +0200

    add a chatbot for twitch

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,27 @@
-var http = require('http').createServer();
-var io = require('socket.io')(http);
+const http = require('http').createServer();
+const io = require('socket.io')(http);
+require('dotenv').config()
+
+const TwitchChatBot = require('./src/chatsbots/twitchChatbot');
+
+const chatBot = new TwitchChatBot(
+  process.env.BOT_ACCOUNT,
+  process.env.AUTH,
+  process.env.STREAM_ACCOUNT
+)
+
+chatBot.connect()
+
+
+chatBot.onMessageRecieved((data) => {
+  console.log(data);
+})
+
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-  });
-  
-  http.listen(3008, () => {
-    console.log('listening on *:3008');
-  });
+  console.log('a user connected');
+});
+
+http.listen(3008, () => {
+  console.log('listening on *:3008');
+});
