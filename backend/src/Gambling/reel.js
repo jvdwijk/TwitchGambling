f8af9e24a0b3e1879fa64b046fad7865f981888d
jvdwijk <joeyvdwijk@gmail.com>
2020-08-25 12:52:43 +0200

    reel update

--- backend/src/Gambling/reel.js
+++ backend/src/Gambling/reel.js
@@ -1,13 +1,31 @@
-const config = require("gamblingConfig.json");
+const config = require("./gamblingConfig.json");
 
 class Reel {
     
-    constructor(){}
+    constructor(reelNumber){
+      this.reelNumber = reelNumber;
+    }
 
     
 
-    _calculateDigitalReel(){
+    _getDigitalReelLength(){
+      let reelSize = 0;
+      for (let i = 0; i < config.reels[this.reelNumber].slots.length; i++) {
+        reelSize += config.reels[this.reelNumber].slots[i].chance;
+      }
 
+      return reelSize;
+    }
+
+    _findPhysicalSlot(slotNumber){
+      let currentPosition = 0;
+      for (let i = 0; i < config.reels[this.reelNumber].slots.length; i++) {
+        currentPosition += config.reels[this.reelNumber].slots[i].chance;
+
+        if(slotNumber > currentPosition){
+          //hap
+        }
+      }
     }
     
   }
