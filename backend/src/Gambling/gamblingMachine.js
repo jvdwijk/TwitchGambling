5f812d37146b3fee05079744c9b3edec04f123fb
jvdwijk <joeyvdwijk@gmail.com>
2020-08-26 15:34:26 +0200

    reel slots

--- backend/src/Gambling/gamblingMachine.js
+++ backend/src/Gambling/gamblingMachine.js
@@ -1,22 +1,21 @@
-const config = require("gamblingConfig.json");
+const config = require("./gamblingConfig.json");
+const reel = require("./reel");
 
 class GamblingMachine {
     
-    constructor(){}
+    constructor(){
+        
+    }
 
     spinGamble(coinValue, payline){
         let reelSlots = [3];
         for (let i = 0; i < 3; i++) {
-            reelCharacters[i] = getReel();
+            
+            
         }
         console.log(`Your results are:  ${reelCharacters[0]} - ${reelCharacters[1]} - ${reelCharacters[2]}`);
             
     }
-
-    _calculateDigitalReel(){
-
-    }
-    
   }
 
 
