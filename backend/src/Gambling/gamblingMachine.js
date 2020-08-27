ab2a10d96e9ed157abec37a707041c1aaee900c4
jvdwijk <joeyvdwijk@gmail.com>
2020-08-27 16:51:04 +0200

    reel info on construct

--- backend/src/Gambling/gamblingMachine.js
+++ backend/src/Gambling/gamblingMachine.js
@@ -1,21 +1,21 @@
 const config = require("./gamblingConfig.json");
-const reel = require("./reel");
+const Reel = require("./reel");
 
 class GamblingMachine {
-    
-    constructor(){
-        
+
+    constructor() {
+        for (let i = 0; i < config.reels.length; i++) {
+            this.reels[i] = new Reel(config.reels[i])
+        }
+        this.spinGamble(5, 5);
     }
 
-    spinGamble(coinValue, payline){
-        let reelSlots = [3];
-        for (let i = 0; i < 3; i++) {
-            
-            
+    spinGamble(coinValue, payline) {
+        for (let i = 0; i < config.reels.length; i++) {
+            console.log(this.reels[i].getDigitalReelLength())
+
         }
         console.log(`Your results are:  ${reelCharacters[0]} - ${reelCharacters[1]} - ${reelCharacters[2]}`);
-            
+
     }
-  }
-
-
+}
--- backend/src/Gambling/reel.js
+++ backend/src/Gambling/reel.js
@@ -1,20 +1,23 @@
-const config = require("./gamblingConfig.json");
-
 class Reel {
 
-    _getDigitalReelLength(reelNumber){
+    constructor(reelInfo){
+      this.reelInfo = reelInfo;
+    }
+
+    getDigitalReelLength(){
       let reelSize = 0;
-      for (let i = 0; i < config.reels[reelNumber].slots.length; i++) {
-        reelSize += config.reels[reelNumber].slots[i].chance;
+      for (let i = 0; i < this.reelInfo.slots.length; i++) {
+        reelSize += this.reelInfo.slots[i].chance;
       }
 
       return reelSize;
     }
 
-    _findPhysicalSlot(reelNumber, slotNumber){
+    _findPhysicalSlot(slotNumber){
       let currentPosition = 0;
-      for (let i = 0; i < config.reels[reelNumber].slots.length; i++) {
-        currentPosition += config.reels[reelNumber].slots[i].chance;
+
+      for (let i = 0; i < this.reelInfo.slots.length; i++) {
+        currentPosition += this.reelInfo.slots[i].chance;
 
         if(slotNumber <= currentPosition){
           return i;
