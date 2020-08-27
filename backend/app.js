c017f3ced872c3967aae6aa20a866fa966705671
Coen Steuns <c.steuns@gmail.com>
2020-08-27 14:17:56 +0200

    setup the connections (clients) for the twitch chat and the front end

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,9 @@
 const http = require('http').createServer();
-const io = require('socket.io')(http);
 require('dotenv').config()
+const io = require('socket.io')(http);
+const SlotMachineGame = require("./src/games/slotmachine/slotMachineGame")
 
-const TwitchChatBot = require('./src/chatsbots/twitchChatbot');
-
-const chatBot = new TwitchChatBot(
-  process.env.BOT_ACCOUNT,
-  process.env.AUTH,
-  process.env.STREAM_ACCOUNT
-)
-
-chatBot.connect()
-
-
-chatBot.onMessageRecieved((data) => {
-  console.log(data);
-})
-
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+const slotMachineGame = new SlotMachineGame('slots', io);
 
 http.listen(3008, () => {
   console.log('listening on *:3008');
--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -1,3 +1,17 @@
 import "../css/main.scss"
 import io from 'socket.io-client';
-const socket = io();
+
+
+const socket = io.connect("ws://localhost:3008/slots/110ec58a-a0f2-4ac4-8393-c866d813b8d1");
+socket.on('error', (err) => {
+    console.log(err);
+});
+
+socket.on('disconnect', (err) => {
+    console.log(err);
+});
+
+socket.on("play-slots", (data, callback) => {
+    console.log(data.user, data.numbers)
+    callback();
+})
